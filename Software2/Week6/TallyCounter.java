1da706d6e51e4373c89691740c472586dc44459e
btso8 <104800987+btso8@users.noreply.github.com>
2022-06-20 23:41:13 +0100

    Add files via upload

---
+++ Software2/Week6/TallyCounter.java
@@ -0,0 +1,58 @@
+package codinguniwork.software2.week6;
+public class TallyCounter {
+
+	private int counter;
+	private int digits;
+
+	public TallyCounter() {
+		counter = 0;
+		digits = 3;
+	}
+
+	public TallyCounter(int digits) throws Exception {
+		if (digits < 3) {
+			throw new Exception("The number of digits must be at least 3!");
+		}
+		counter = 0;
+		this.digits = digits;
+	}
+
+	private int computeMaxValue() {
+		return (int) Math.pow(10, digits) - 1;
+	}
+
+	public int decrement() {
+		int old = counter;
+		counter = counter > 0 ? counter - 1 : 0;
+		return old;
+
+	}
+
+	public int increment() {
+		int old = counter;
+		if (counter >= computeMaxValue()) {
+			counter = 0;
+		} else {
+			counter++;
+		}
+		return old;
+	}
+
+	public int reset() {
+		int old = counter;
+		counter = 0;
+		return old;
+
+	}
+
+	public int read() {
+		return counter;
+	}
+
+	public String toString() {
+		String template = "%0";
+		template += digits + "d";
+		return String.format(template, counter);
+	}
+
+}
