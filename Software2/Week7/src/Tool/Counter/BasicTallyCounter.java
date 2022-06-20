1604f8fbb28c99ac00ceb690ae088a9ea5791116
btso8 <104800987+btso8@users.noreply.github.com>
2022-06-20 23:48:35 +0100

    Create BasicTallyCounter.java

---
+++ Software2/Week7/src/Tool/Counter/BasicTallyCounter.java
@@ -0,0 +1,56 @@
+package University-Code.Software2.Week6.src.Tool.Counter;
+
+public class BasicTallyCounter implements ITallyCounter{
+
+	int counter;
+	int digits;
+
+	public BasicTallyCounter() {
+		counter = 0;
+		digits = 3;
+	}
+
+	public BasicTallyCounter(int digits) throws Exception {
+		if (digits < 3) {
+			throw new Exception("The number of digits must be at least 3!");
+		}
+		counter = 0;
+		this.digits = digits;
+	}
+
+	private int computeMaxValue() {
+		return (int) Math.pow(10, digits) - 1;
+	}
+
+	@Override
+	public int increment() throws InvalidOperationException {
+		int old = counter;
+		if (counter >= computeMaxValue()) {
+			throw new InvalidOperationException(InvalidOperationException.INCREMENT_OPERATION);
+		} else {
+			counter++;
+		}
+		return old;
+	}
+
+	@Override
+	public int reset() {
+		int old = counter;
+		counter = 0;
+		return old;
+
+	}
+
+	@Override
+	public int read() {
+		return counter;
+
+	}
+
+	public String toString() {
+		String template = "%0";
+		template += digits + "d";
+		return String.format(template, counter);
+	}
+
+}
