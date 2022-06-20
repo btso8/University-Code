a9252554739e1571e6518c0868eb3f6a27ef8ea8
btso8 <104800987+btso8@users.noreply.github.com>
2022-06-21 00:00:11 +0100

    Add files via upload

---
+++ Software2/Week9/src/App.java
@@ -0,0 +1,7 @@
+package codinguniwork.software2.week9.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
