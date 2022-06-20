929be31ccdf7a693b0a1e564a81f487494017027
btso8 <104800987+btso8@users.noreply.github.com>
2022-06-20 23:44:28 +0100

    Create IncompatibleDimensionException.java

---
+++ Software2/Week7/src/Algebra/IncompatibleDimensionException.java
@@ -0,0 +1,9 @@
+package University-Code.Software2.Week6.src.Algebra;
+
+public class IncompatibleDimensionException extends Exception{
+
+    public IncompatibleDimensionException(String message){
+        super(message);
+    }
+    
+}
